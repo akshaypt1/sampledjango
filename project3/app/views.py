e0c8716729f08e259b52d5b1f348f8ff25e09b3e
akshaypt1 <akshaypt010@gmail.com>
2024-11-25 21:01:06 +0530

    work

--- project3/app/views.py
+++ project3/app/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        print(username,password)
     return render(request,'index.html')
