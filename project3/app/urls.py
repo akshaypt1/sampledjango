586d322d619962ebaa84153fc21e35ad12fd1515
rijuroy21 <rijumonroy@gnail.com>
2024-11-26 11:33:12 +0530

    work

--- project3/app/urls.py
+++ project3/app/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from.import views
 urlpatterns = [
     path('',views.index),
+    path('login',views.index2),
+    path('home',views.home)
 ]
--- project3/app/views.py
+++ project3/app/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+user=[]
 def index(request):
     if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
         username=request.POST['username']
         password=request.POST['password']
-        print(username,password)
+        user.append([name,email,username,password])
+        print(user)
     return render(request,'index.html')
+def index2(request):
+    if request.method=='POST':
+        username=request.POST['usname']
+        password=request.POST['psword']
+        for i in user:
+            if username==i[2] and password==i[3]:
+                print([i[0]])
+                return redirect(home)
+    return render(request,'index2.html')
+def home(request):
+    return render(request,'home.html')
+                
+        
