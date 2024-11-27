dbfdaa02acf30b5b568aacea22c031622008495b
rijuroy21 <rijumonroy@gnail.com>
2024-11-27 11:38:05 +0530

    work

--- project3/app/urls.py
+++ project3/app/urls.py
@@ -3,5 +3,8 @@ from.import views
 urlpatterns = [
     path('',views.index),
     path('login',views.index2),
-    path('home',views.home)
+    path('home',views.home),
+    path('admlogin',views.adminlogin),
+    path('adminhome',views.adminhome),
+    
 ]
--- project3/app/views.py
+++ project3/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 
 # Create your views here.
+admin_usname='admin'
+admin_psword='admin123'
 user=[]
 def index(request):
     if request.method=='POST':
@@ -22,5 +24,15 @@ def index2(request):
     return render(request,'index2.html')
 def home(request):
     return render(request,'home.html')
+def adminlogin(request):
+    if request.method=='POST':
+        admin_usname=request.POST['usname']
+        admin_pswod=request.POST['psword']
+        if admin_usname==admin_usname and admin_pswod==admin_pswod:
+            return redirect(adminhome)
+    return render(request,'adminlogin.html')
+def adminhome(request):
+    return render(request,'adminhome.html',{'users':user})
+    
                 
         
